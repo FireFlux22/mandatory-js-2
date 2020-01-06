a836070bbd7a5c3b499a2d5890ce471468c28b59
Karoline <karolineph@gmail.com>
2020-01-06 11:32:24 +0100

    Add winning scenario

--- script.js
+++ script.js
@@ -32,18 +32,43 @@ function clickBox() {
     }
 }
 
-var winningCombinations = [
+let winningCombinations = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ];
 
+document.addEventListener("click", rowInLine);
+
+
+function rowInLine() {
+
+    winningCombinations.forEach(function (containComb) {
+        let positionZero = boxes[containComb[0]].textContent;
+        let positionOne = boxes[containComb[1]].textContent;
+        let positionTwo = boxes[containComb[2]].textContent;
+
+        winner = positionZero !== '' && positionZero === positionOne && positionOne === positionTwo;
+
+        if (winner === true) {
+            // ta bort möjligheten att klicka
+            boxes.forEach((box) => {
+                box.removeEventListener("click", clickBox);
+            })
+
+            document.getElementById("message").textContent = positionZero + " is the winner!";
+        }
+    });
+}
+
 document.getElementById("button").addEventListener("click", resetGame);
 
-function resetGame () {
-    boxes.forEach( (box) => {
+function resetGame() {
+    boxes.forEach((box) => {
         box.textContent = "";
+        box.addEventListener("click", clickBox);
+        document.getElementById("message").textContent = "";
     }),
 
-    turn = 0;
+        turn = 0;
 }
